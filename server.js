2a970b3b8ffbca9d62b712d0a73cf50b34ffb4fa
ravi <rprakash262@gmail.com>
2022-08-21 09:40:49 +0530

    ssfgr

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ mongoose
 
 app.use('/', api);
 
+  app.use(express.static('client/build'));
 // serve static asset if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
