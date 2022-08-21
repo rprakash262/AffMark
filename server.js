3f209fc6bde21e38ce49bb3ca3bb8c1145e831ad
ravi <rprakash262@gmail.com>
2022-08-21 10:10:48 +0530

    ssfgr

--- server.js
+++ server.js
@@ -23,16 +23,14 @@ mongoose
 app.use('/', api);
 
 // serve static asset if in production
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  // app.use(express.static('client/build'));
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
-}
+// }
 
 const PORT = process.env.PORT || 5000;
 
